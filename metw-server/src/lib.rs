602a21b628b3a27c3d10fee643f3e4df533deed2
Metehan Selvi <me@metehanselvi.com>
2026-08-23 23:46:17 +0000

    feat: `metw-server` for simple axum server

---
+++ metw-server/src/lib.rs
@@ -0,0 +1,59 @@
+//! # metw-server
+//!
+//! Serve axum application with good defaults.
+//!
+//!
+//! ## Cargo features
+//!
+//! - `otel`: Enables OTLP/gRPC trace export.
+//! - `allow-all-cors`: Allows CORS for all hosts.
+
+use axum::Router;
+use std::{env, net::SocketAddr};
+use tokio::signal;
+
+/// Serve the application.
+pub async fn serve(app: Router, service_name: String) {
+    metw_observability::init_tracing(service_name);
+
+    let sock_addr: SocketAddr = env::var("HOST")
+        .unwrap_or_else(|_| {
+            println!("HOST environment variable is not set, defaulting to 127.0.0.1:3781");
+
+            "127.0.0.1:3781".to_string()
+        })
+        .parse()
+        .unwrap();
+
+    let listener = tokio::net::TcpListener::bind(&sock_addr).await.unwrap();
+
+    #[cfg(feature = "allow-all-cors")]
+    let app = {
+        use tower_http::cors::{AllowHeaders, AllowMethods, Any, CorsLayer};
+
+        let cors = CorsLayer::new()
+            .allow_methods(AllowMethods::any())
+            .allow_origin(Any)
+            .allow_headers(AllowHeaders::any());
+
+        app.layer(cors)
+            .layer(metw_observability::trace_layer_for_http())
+    };
+    #[cfg(not(feature = "allow-all-cors"))]
+    let app = app.layer(metw_observability::trace_layer_for_http());
+
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
+        .await
+        .unwrap();
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler")
+    };
+
+    ctrl_c.await
+}
